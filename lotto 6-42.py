1ce4d363f7db0a40fff2bc4de2afba9a0aa82338
Peter-bud <budzinskuypetro1990@i.ua>
2020-03-04 20:39:28 +0200

    alpha

--- lotto 6-42.py
+++ lotto 6-42.py
@@ -1,18 +1,16 @@
 import csv
+from random import sample, choice
 
 
 def main():
     temp_list = []
     all_data = []
 
-
-
     with open('big_data.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
 
         for row in spamreader:
             temp_list.append((', '.join(row)))
-
 
         for i in temp_list:
             new_int = int(i)
@@ -22,36 +20,22 @@ def main():
     del all_data[6::7]
     chance = []
 
-    for i in range(0,42):
-        chance.append((all_data.count(i))/len(all_data)*100)
+    for i in range(0, 42):
+        chance.append((all_data.count(i)) / len(all_data) * 100)
 
-
-
-    average= sum(chance)/len(chance)
-    min_list=[]
-    max_list=[]
+    average = sum(chance) / len(chance)
+    min_list = []
+    max_list = []
     for index, item in enumerate(chance):
 
-        if item<average:
-            print(index+1,item)
-            min_list.append(index+1)
-        elif item>=average:
-            max_list.append(index+1)
+        if item < average:
 
-    print(max_list)
-    print(min_list)
+            min_list.append(index + 1)
+        elif item >= average:
+            max_list.append(index + 1)
 
-
-
-
+    print('First attempt: ', sample(min_list, 6), 'Superball', '', choice(super_ball))
+    print('The second attempt: ', sample(max_list, 6), 'Superball', '', choice(super_ball))
 
 
 main()
-
-
-
-
-
-
-
-
