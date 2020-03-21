d1ae3c7a42facb341c7bd188fe765b68864542d7
Peter-Bud <Budzinskuy_petro@i.ua>
2020-03-21 18:24:14 +0200

    add requests function

--- lotto 6-42.py
+++ lotto 6-42.py
@@ -1,9 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
 import csv
 from random import sample, choice
 
+URL='https://igra.msl.ua/megalote/uk/archive'
+HEADERS={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36',
+         'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'}
 
-def parser():
-    pass
+def get_html(url, params=None):
+    req=requests.get(url, headers=HEADERS, params=params)
+    return req
+
+
+
+def parse():
+    html=get_html(URL)
+    print(html)
+
+parse()
 
 
 def transform():
