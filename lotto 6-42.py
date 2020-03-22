bfe121f11452acd49aa27a2a85c9f016d0faccaf
Peter-Bud <Budzinskuy_petro@i.ua>
2020-03-22 15:25:26 +0200

    add some description

--- lotto 6-42.py
+++ lotto 6-42.py
@@ -32,45 +32,50 @@ parse()
 
 
 def transform():
-    temp_list = []
-    all_data = []
+    """get csv file and return 2 lists with numbers"""
+    temp_list = []  # empty list for temporary computation within function
+    all_data = []  # empty list for adding all numbers from csv file
 
     with open('big_data.csv', newline='') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')  # open csv file
 
         for row in spamreader:
-            temp_list.append((', '.join(row)))
+            temp_list.append((', '.join(row)))  # add all numbers from csv file to temp_list
 
         for i in temp_list:
             new_int = int(i)
-            all_data.append(new_int)
+            all_data.append(new_int)  # Transform all numbersfrom str to int and add to all_data list
 
-    super_ball = all_data[6::7]
-    del all_data[6::7]
+    super_ball = all_data[6::7]  # determing super ball (every 7 numbers
+    del all_data[6::7]  # deleting superball from all plural
     return all_data, super_ball
 
 
 def calculate():
-    all_data, super_ball = transform()
-    chance = []
+    """func gets all_data and superball lists and return min_list and max_lists where contain numbers
+    with minimum and maximum chance of fall out by diving chance of occur to average chance of occur. """
+    all_data, super_ball = transform()  # call transform func and assign its value to variable
+    chance = []  # empty list for calculating hance of chance of falling out
 
-    for i in range(0, 42):
-        chance.append((all_data.count(i)) / len(all_data) * 100)
-    average = sum(chance) / len(chance)
-    min_list = []
-    max_list = []
+    for i in range(0, 42):  # 0-42 posible total numbers
+        chance.append((all_data.count(i)) / len(all_data) * 100)    #calculate chance of fall out by iterating dividing occurence 1-42 numbers to len all data list.
+    average = sum(chance) / len(chance)     #determine average chance of fall out for all 1-42 numbers
+    min_list = []       #emty list for further adding numbers which chance of fall are lower than average
+    max_list = []       #emty list for further adding numbers which chance of fall are bigger than average
 
     for index, item in enumerate(chance):
 
         if item < average:
-            min_list.append(index + 1)
+            min_list.append(index + 1)  #determine and adding to min list numbers which lower chance to fall out than average chance
+
         elif item >= average:
-            max_list.append(index + 1)
+            max_list.append(index + 1)  #determine and adding to max list numbers which bigger chance to fall out than average chance
 
     return min_list, max_list, super_ball
 
 
 def result():
+    '''Get min and max list and print 6 numbers from each list by random + 1 number from super_ball list'''
     min_list, max_list, super_ball = calculate()
     print('First attempt: ', sample(min_list, 6), 'Superball', '', choice(super_ball))
     print('The second attempt: ', sample(max_list, 6), 'Superball', '', choice(super_ball))
